bd7972726b369c76c35eb6c792254d5753b72d86
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-09-08 10:15:38 -0700

    1. Compiler - 8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase "Programming is like building a  multilingual pizzle"
+ * Return: 0 if exited properly, non-zer0 otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the phrase with propergrammar,
+ * but the outcome is a piece of art,
+ * Return; 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammer, but outcome is a piece of art,\n");
+	return (0);
+}
