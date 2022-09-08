695cc6d255199fbf2067925249e66b6c625528ac
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-09-08 11:36:12 -0700

    Fixed a "line over 80 characters (82) bug in file 4-puts.c" This was resolved using betty. The command betty 4-puts.c game showed me I had an extra whitespace the madd my caharcters (82) long instead of (80).

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "Programming is like building a  multilingual pizzle"
+ * main - prints the phrase "Programming is like building a multilingual pizzle
  * Return: 0 if exited properly, non-zer0 otherwise
  */
 int main(void)
