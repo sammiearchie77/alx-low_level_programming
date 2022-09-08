1a4af558eab2d80690c4bc21a79f53170556aad4
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-09-08 11:42:04 -0700

    Fixed a "line over 80 characters (82) bug in file 4-puts.c" This was resolved using betty. The command betty 4-puts.c game showed me I had an extra whitespace the madd my caharcters (82) long instead of (80).

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - prints the phrase with propergrammar,
  * but the outcome is a piece of art,
- * Return; 0 if exited properly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
