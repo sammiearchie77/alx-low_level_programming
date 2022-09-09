081c8fbaed60c6821c3681165d8545b473a3ae90
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-09-09 16:41:09 -0700

    Solved from 1. The last digit - 12. Software is eating the World

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers od base 16 in lowercase,
+ * follwed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
