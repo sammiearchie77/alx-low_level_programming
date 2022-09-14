cc4effdf8109d42c580a2291674a7427ec380166
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-09-14 07:44:35 -0700

    Debugging print_remaining_days

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * REturn: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/**
+	 * while (i < 10)
+	 * {
+	 *     putchar(i);
+	 * }
+	 */
+
+	printf("Infinte loop avoided! \\o/\n");
+
+	return (0);
+}
