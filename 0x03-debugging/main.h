1c9915cf26f5a62b6420510bd4930fec75625761
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-09-15 02:07:12 -0700

    Still Debuggin

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
@@ -6,3 +9,4 @@ void print_remaining_days(int, int, int);
 int convert_day(int, int);
 
 #endif /* MAIN_H */
+extern int make_iso_compilers_happy;
