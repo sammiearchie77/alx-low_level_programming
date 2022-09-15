5f33cb52a7c9d727e10c6c72812b124ff78d08d1
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-09-15 13:17:10 -0700

    Solution 2 and 3

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * mul - multiples two numbers
  * @a: first number
  * @b: second number
+ *
  * REturn: return result
  */
 
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -15,5 +14,5 @@ void print_numbers(void)
 	{
 		putchar(n);
 	}
-	putchar(10);
+	putchar('\n');
 }	
