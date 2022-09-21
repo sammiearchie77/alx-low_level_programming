74b3afe83c306d384762a5e208c074b1da41cc01
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-09-21 16:38:31 -0700

    Solutions from 2. strncpy to 7-leet.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,6 @@
  * _strcat - concatenates two strings
  * @dest: string to append to
  * @src: string to add
- *
  * Return: a pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
