34312130e22e7e92ea8d87257fdfe7148201c354
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-09-22 04:41:10 -0700

    Solutions for Infinite_add and Print Buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,31 +2,77 @@
 #include <stdio.h>
 
 /**
- *  print_line - prints a s bytes of a buffer
- *  @c: buffer to print
- *  @s: bytes of buffer to print
- *  @l: line of buffer to print
- *
- *  Return: void
+ * isPrintableASCII - determines if n is a printable ASCII char
+ * @n: integer
+ * Return: 1 if true, 0 if false
+ */
+int isPrintableASCII(int n)
+{
+	return (n >= 32 && n <= 126);
+}
+
+/**
+ * printHexes - print hex values for string b in formatted form
+ * @b: string to print
+ * @start: starting position
+ * @end: ending position
+ */
+void printHexes(char *b, int start, int end)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i < end)
+			printf("%02x", *(b + start + i));
+		else
+			printf("  ");
+		if (i % 2)
+			printf(" ");
+		i++;
+	}
+}
+
+/**
+ * printASCII - print ascii values for string b,
+ * formatted to replace nonprintable chars with '.'
+ * @b: string to print
+ * @start: starting position
+ * @end: ending position
+ */
+void printASCII(char *b, int start, int end)
+{
+	int ch, i = 0;
+
+	while (i < end)
+	{
+		ch = *(b + i + start);
+		if (!isPrintableASCII(ch))
+			ch = 46;
+		printf("%c", ch);
+		i++;
+	}
+}
+
+/**
+ * print_buffer - prints a buffer
+ * @b: string
+ * @size: size of buffer
  */
 void print_buffer(char *b, int size)
 {
-	int a, b;
+	int start, end;
 
-	for (a = 0; a <= 9; a++)
+	if (size > 0)
 	{
-		if (a <= s)
-			rintf("%02x", c[l * 10 + a]);
-		else
-			printf("  ");
-		if (a % 2)
-			putchar(' ');
-	}
-	for (b = 0; b <= s; b++)
-	{
-		if (c[l * 10 + b] > 31 && c[l * 10 + b] < 127)
-			putchar(c[l * 10 + b]);
-		else
-			putchar('.');
-	}
+		for (start = 0; start < size; start += 10)
+		{
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x: ", start);
+			printHexes(b, start, end);
+			printASCII(b, start, end);
+			printf("\n");
+		}
+	} else
+		printf("\n");
 }
