3aa8de0b69ead6ee9a210d3928e68a3d4f66d0c0
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-09-21 17:38:58 -0700

    File 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -30,31 +30,3 @@ void print_buffer(char *b, int size)
 			putchar('.');
 	}
 }
-
-/**
- * print_buffer - prints a buffer
- * @b: buffer to print
- * @size: size of buffer
- *
- * Return: void
- */
-void print_buffer(char *b, int size)
-{
-	int m;
-
-	for (m = 0; m <= (size - 1) / 10 && size; m++)
-	{
-		printf("%08x: ", m * 10);
-		if (m < size / 10)
-		{
-			print_line(b, 9, m);
-		}
-		else
-		{
-			print_line(b, size % 10 - 1, m);
-		}
-		putchar('\n');
-	}
-	if (size == 0)
-		putchar('\n');
-}
