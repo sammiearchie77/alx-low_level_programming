b655242785ac6298cd39377f4474e9044af55040
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-09-22 13:45:13 -0700

    103-infinite.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * infinite_add - adds two numbers
- * @n1: number one.
- * @n2: number two.
- * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
- * Return: the pointer to dest.
+ * @n1: number one
+ * @n2: number two
+ * @r: buffer that the function will use to store the result
+ * @size_r: buffer size
+ * Return: the pointer to dest
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
