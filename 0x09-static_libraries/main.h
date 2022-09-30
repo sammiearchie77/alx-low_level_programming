5700dd74e99bab989e766f69f3506b2f93c11d1e
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-09-30 08:52:31 -0700

    Build your own C static library

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_
+#define MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -8,7 +8,7 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *s);
+int _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
