f1cab8e9fce704fcbc8df50bcab80978008a336d
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-10-27 13:06:41 -0700

    debugged 5-fib_bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -16,7 +16,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	for (i = 63; i >= 0; i--)
 	{
-		current = exclusive >> 1;
+		current = exclusive >> i;
 		if (current & 1)
 			count++;
 	}
