bb06b5610ee44880265e90e1c851f882d271513f
sammiearchie77 <chinonsosamuel29@gmail.com>
2023-01-05 07:30:04 -0800

    Hash table done

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index: Get the index at which a key/value pair should
+ *            be stored in array of a hash tablle
+ * @key: The key ot get the index of.
+ * @size: The size of the array of the hash table.
+ *
+ * Return: The index of the key.
+ *
+ * Description: Using the djd2 algorithm.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djd2(key) % size);
+}
